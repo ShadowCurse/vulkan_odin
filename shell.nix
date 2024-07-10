b2ca741a0376dc2667087959a0a36f170700c5b0
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-10 23:25:12 +0100

    add necessary vulkan variables

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
+  LD_LIBRARY_PATH="${pkgs.glfw}/lib:${pkgs.vulkan-loader}/lib:${pkgs.vulkan-validation-layers}/lib";
+  VULKAN_SDK = "${pkgs.vulkan-headers}";
+  VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+
   buildInputs = with pkgs; [
     odin
     ols
     glfw
+    vulkan-tools
+    vulkan-loader
+    vulkan-headers
+    vulkan-validation-layers
     pkg-config
   ];
 }
